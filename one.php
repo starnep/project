c859fb13e1ca85798ce117002d163f84eb3af9e6
starnep <natnicha.thitsuksai@gmail.com>
2018-07-13 09:56:12 +0700

    connect db

---
+++ one.php
@@ -0,0 +1,25 @@
+<?php 
+$host="localhost";
+$user="root";
+$pass="";
+$dbname="project";
+
+$con=mysqli_connect($host,$user,$pass,$dbname);
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+mysqli_set_charset($con,"utf8");
+
+?>
+
+
+
+<!DOCTYPE>
+<html>
+<head>
+	<title>project</title>
+</head>
+<body>
+
+</body>
+</html>
